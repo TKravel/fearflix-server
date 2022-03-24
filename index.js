2c1b9b34e7fde3e1bdfc644dea0d08b0d19f3e26
Tim Kravel <TKravel@Yahoo.com>
2022-03-24 13:42:14 -0400

    create func to get total pages of movies, get all movies, send them to client

--- index.js
+++ index.js
@@ -1,16 +1,68 @@
 require('dotenv').config();
 const express = require('express');
 const fetch = require('node-fetch');
-const PORT = 3000;
+const cors = require('cors');
+const res = require('express/lib/response');
+const PORT = 3001;
 
 const app = express();
 
-fetch('https://api.github.com/users/github')
-	.then((res) => res.json())
-	.then((json) => console.log(json));
+const corsOptions = {
+	origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOptions));
+
+const fetchMovies = async () => {
+	let movies = [];
+	let page = 2;
+	let maxPages = null;
+
+	let url = `https://streaming-availability.p.rapidapi.com/search/pro?country=us&service=netflix&type=movie&order_by=original_title&genre=27&page=${page}&desc=true&language=en&output_language=en`;
+
+	let options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com',
+			'X-RapidAPI-Key': process.env.MOVIE_API_KEY,
+		},
+	};
+
+	const fetchRemainingPages = async () => {
+		for (let i = page; i <= maxPages; i++) {
+			console.log(i);
+			await fetch(url, options)
+				.then((res) => res.json())
+				.then((json) => {
+					movies.push(...json.results);
+					page = page + 1;
+				})
+				.catch((err) => console.error('error:' + err));
+		}
+	};
+
+	await fetch(url, options)
+		.then((res) => res.json())
+		.then((json) => {
+			movies.push(...json.results);
+			maxPages = json.total_pages;
+			console.log(movies);
+			console.log(maxPages);
+		})
+		.catch((err) => console.error('error:' + err));
+
+	await fetchRemainingPages();
+	console.log(movies.length);
+	return movies;
+};
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+app.get('/test', async (req, res) => {
+	const movieData = await fetchMovies();
+	res.json({ data: movieData });
 });
 
 app.listen(PORT, () => {
