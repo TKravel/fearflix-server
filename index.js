b5cf446b3e74dfa70293342cf03dc687e90f9491
Tim Kravel <TKravel@Yahoo.com>
2022-03-24 11:42:37 -0400

    require fetch

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const fetch = require('node-fetch');
 const PORT = 3000;
 
 const app = express();
+
+fetch('https://api.github.com/users/github')
+	.then((res) => res.json())
+	.then((json) => console.log(json));
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
